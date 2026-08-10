0a3e95206003e2f15b650ed91ca87e4b17ffc6cb
Kittiphong-Cyber <kittiphong064008@gmail.com>
2026-08-10 17:19:04 +0700

    change map to struct in json response

--- pkg/products/dto/response.go
+++ pkg/products/dto/response.go
@@ -8,8 +8,32 @@ type Products struct {
 }
 
 type ProductResponse struct {
-	TotalItem int
-	MaxPrice  int
-	MinPrice  int
+	TotalItem int `json:"Total"`
+	MaxPrice  int `json:"Max"`
+	MinPrice  int `json:"Min"`
 	Products  []Products
+}
+
+type ResponseProducts struct {
+	Message string          `json:"message"`
+	Value   ProductResponse `json:"products"`
+}
+
+type ResponseOne struct {
+	Message string   `json:"message"`
+	Value   Products `json:"products"`
+}
+
+type ResponseAll struct {
+	Message string     `json:"message"`
+	Value   []Products `json:"products"`
+}
+
+type ResponseRows struct {
+	Message    string `json:"message"`
+	RowsAffect int    `json:"RowsAffect"`
+}
+
+type ResponseError struct {
+	Message string `json:"message"`
 }
--- pkg/products/handler/handler.go
+++ pkg/products/handler/handler.go
@@ -21,67 +21,67 @@ func NewHandler(service service.Service) *handler {
 func (h *handler) CreateProducts(c fiber.Ctx) error {
 	var req dto.ProductsRequest
 	if err := c.Bind().Body(&req); err != nil {
-		return c.Status(500).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(400).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	_, err := h.service.CreateProducts(c.Context(), req)
+	res, err := h.service.CreateProducts(c.Context(), req)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(201).JSON(fiber.Map{"Message": "Create Products Success"})
+	return c.Status(201).JSON(dto.ResponseOne{Message: "Create Success", Value: *res})
 }
 
 func (h *handler) GetProductByid(c fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(400).JSON(dto.ResponseError{Message: err.Error()})
 	}
 	res, err := h.service.GetProductByid(c.Context(), id)
 	if err != nil {
-		return c.Status(501).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(200).JSON(fiber.Map{"Message": "GET Success", "Value": res})
+	return c.Status(200).JSON(dto.ResponseOne{Message: "GET Success", Value: *res})
 }
 
 func (h *handler) GetAllProducts(c fiber.Ctx) error {
 	res, err := h.service.GetAllProducts(c.Context())
 	if err != nil {
-		return c.Status(501).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(200).JSON(fiber.Map{"Message": "GET Success", "Value": res})
+	return c.Status(200).JSON(dto.ResponseAll{Message: "GET Success", Value: *res})
 }
 
 func (h *handler) UpdateProductsById(c fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(400).JSON(dto.ResponseError{Message: err.Error()})
 	}
 	var p dto.ProductsRequest
 	if err := c.Bind().Body(&p); err != nil {
-		return c.Status(500).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(400).JSON(dto.ResponseError{Message: err.Error()})
 	}
 	rowsAffected, err := h.service.UpdateProductsById(c.Context(), id, p)
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(200).JSON(fiber.Map{"Message": "Update Products Success", "rowsAffected": rowsAffected})
+	return c.Status(200).JSON(dto.ResponseRows{Message: "Update Success", RowsAffect: rowsAffected})
 }
 
 func (h *handler) DeleteProductsById(c fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(400).JSON(dto.ResponseError{Message: err.Error()})
 	}
 	rowsAffected, err := h.service.DeleteProductsById(c.Context(), id)
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(200).JSON(fiber.Map{"Message": "Delete Products Sucess", "rowsAffected": rowsAffected})
+	return c.Status(200).JSON(dto.ResponseRows{Message: "Delete Success", RowsAffect: rowsAffected})
 }
 
 func (h *handler) GetProductsItems(c fiber.Ctx) error {
 	res, err := h.service.GetProductsItems(c.Context())
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"Message": err.Error()})
+		return c.Status(500).JSON(dto.ResponseError{Message: err.Error()})
 	}
-	return c.Status(200).JSON(fiber.Map{"Message": "GET Success", "Value": res})
+	return c.Status(200).JSON(dto.ResponseProducts{Message: "Get Success", Value: *res})
 }
